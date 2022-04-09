9d7caa425aac8963dcebf5ad14bd98c0cb465974
mohamed <mohamed.baioumy1@outlook.com>
2022-04-09 12:08:16 -0300

    add code to js.js

--- js.js
+++ js.js
@@ -5,7 +5,14 @@
         const controlImage = $(this);
         var images = $("#container");
         const firstImage = 0;
+        var screenLeft = $("#container", this);
+
+
         screenLeft.click(evt())({
+            firstImage = (images + images - 1) % images;
+            $("#container", slideshow)
+
+
             
         });
 
