88e015e7a0323200694919ec2d4646efd7ad894f
mohamed <mohamed.baioumy1@outlook.com>
2022-04-10 10:30:32 -0300

    add code to js.js

--- js.js
+++ js.js
@@ -6,27 +6,19 @@
         var images = $("#container");
         const firstImage = 0;
         var screenLeft = $("#container", this);
+        $("#container").click( evt => {
+            const link = evt.currentTarget;
+            
+            const imageURL = $(link).attr("href");
+            $("#image").attr("src", imageURL);
+            $("#image").fadeOut(1000,()=>{
+            $("#image").attr("src", imageURL).fadeIn(1000);
+            });		
+            
+        });
 
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })(jQuery);
