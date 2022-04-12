8ad5cb0360c2a5c16e9309e16da48e3f18cd9786
mohamed <mohamed.baioumy1@outlook.com>
2022-04-11 22:25:50 -0300

    add html code

--- js.js
+++ js.js
@@ -1,17 +1,39 @@
 (function ($) {
 
-    $.fn.controlImage = function(){
-
-        $.fn.slider = function (Options) {
-            let settings = $.extend(
-              {
-                fadeOut: 1000,
-                fadeIn: 1000,
-                time:3000
-              },
-              Options
-            );
-        
+    $.fn.slider = function (Options) {
+        let settings = $.extend(
+            {
+            fadeOut: 1000,
+            fadeIn: 1000,
+            time:3000
+            },
+            Options
+        );
+    this.each(function () {
+        var slideshow = $(this);
+    
+        var num_slides = $(".slide", this).length;
+    
+        var current_slide = 0;
+    
+        var prev_btn = $(".slideshow-control[rel='prev']", this);
+        var interval = null;
+        prev_btn.click(function () {
+    
+            current_slide = (current_slide + num_slides - 1) % num_slides;
+    
+            $(".slide.current", slideshow)
+    
+            .fadeOut(settings.fadeOut, function () {
+    
+                $(".slide:eq(" + current_slide + ")", slideshow)
+                
+                .fadeIn(settings.fadeIn)
+    
+                .addClass("current");
+            })
+            .removeClass("current");
+        });
 
 
     }
