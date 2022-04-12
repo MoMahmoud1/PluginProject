0e6e0972cbe4b74efd0fceee170865d1fd2cf72d
mohamed <mohamed.baioumy1@outlook.com>
2022-04-12 18:12:15 -0300

    add html code

--- js.js
+++ js.js
@@ -2,9 +2,11 @@
     $.fn.slider = function (Options) {
       let settings = $.extend(
         {
+
           fadeOut: 1000,
           fadeIn: 1000,
           time:3000
+          
         },
         Options
       );
