b4385dcfb42ae15e1a6e45d50bb91183ab8fbd96
mohamed <mohamed.baioumy1@outlook.com>
2022-04-11 22:17:41 -0300

    add lins of code

--- js.js
+++ js.js
@@ -2,21 +2,16 @@
 
     $.fn.controlImage = function(){
 
-        const controlImage = $(this);
-        var images = $("#container");
-        const firstImage = 0;
-        var screenLeft = $("#container", this);
-        $("#container").click( evt => {
-            const link = evt.currentTarget;
-            
-            const imageURL = $(link).attr("href");
-            $("#image").attr("src", imageURL);
-            $("#image").fadeOut(1000,()=>{
-            $("#image").attr("src", imageURL).fadeIn(1000);
-            });		
-            
-        });
-
+        $.fn.slider = function (Options) {
+            let settings = $.extend(
+              {
+                fadeOut: 1000,
+                fadeIn: 1000,
+                time:3000
+              },
+              Options
+            );
+        
 
 
     }
