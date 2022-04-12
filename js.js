18563be7e4c1403b9297259235865bc4487ecf2f
mohamed <mohamed.baioumy1@outlook.com>
2022-04-12 10:02:25 -0300

    add lines of code

--- js.js
+++ js.js
@@ -1,41 +1,99 @@
 (function ($) {
-
     $.fn.slider = function (Options) {
-        let settings = $.extend(
-            {
-            fadeOut: 1000,
-            fadeIn: 1000,
-            time:3000
-            },
-            Options
-        );
-    this.each(function () {
+      let settings = $.extend(
+        {
+          fadeOut: 1000,
+          fadeIn: 1000,
+          time:3000
+        },
+        Options
+      );
+  
+      this.each(function () {
         var slideshow = $(this);
-    
+  
         var num_slides = $(".slide", this).length;
-    
+  
         var current_slide = 0;
-    
-        var prev_btn = $(".slideshow-control[rel='prev']", this);
+  
+        var prevuse_click = $(".slideshow-control[rel='prev']", this);
         var interval = null;
-        prev_btn.click(function () {
-    
-            current_slide = (current_slide + num_slides - 1) % num_slides;
-    
-            $(".slide.current", slideshow)
-    
+        prevuse_click.click(function () {
+  
+          current_slide = (current_slide + num_slides - 1) % num_slides;
+  
+          $(".slide.current", slideshow)
+  
             .fadeOut(settings.fadeOut, function () {
-    
-                $(".slide:eq(" + current_slide + ")", slideshow)
-                
+  
+              $(".slide:eq(" + current_slide + ")", slideshow)
+              
                 .fadeIn(settings.fadeIn)
-    
+  
                 .addClass("current");
             })
             .removeClass("current");
         });
-
-
-    }
-
-})(jQuery);
+  
+        var next_click = $(".slideshow-control[rel='next']", this);
+  
+        next_click.click(function () {
+          current_slide = (current_slide + num_slides + 1) % num_slides;
+          $(".slide.current", slideshow)
+            .fadeOut(settings.fadeOut, function () {
+              $(".slide:eq(" + current_slide + ")", slideshow)
+                .fadeIn(settings.fadeIn)
+  
+                .addClass("current");
+            })
+  
+            .removeClass("current");
+        });
+        var auto = $(".auto-control[rel='auto']", this);
+        auto.click(function () {
+          // for(let i = 0; i < num_slides; i++){
+          //   const nextImage = num_slides[current_slide];
+  
+          //     $(".slide", slideshow)
+  
+          //     .fadeOut(settings.fadeOut, function () {
+  
+          //       $(".slide:eq(" + nextImage + ")", slideshow)
+  
+          //       .fadeIn(settings.fadeIn)
+  
+          //       .addClass("current");
+          //     })
+  
+          //     .removeClass("current");
+  
+          // }
+  
+          interval =  setInterval(() => {
+            current_slide = (current_slide + num_slides + 1) % num_slides;
+            $(".slide.current", slideshow)
+  
+              .fadeOut(settings.fadeOut, function () {
+  
+                $(".slide:eq(" + current_slide + ")", slideshow)
+                  .fadeIn(settings.fadeIn)
+  
+                  .addClass("current");
+              })
+  
+              .removeClass("current");
+          });
+        }, settings.time);
+  
+        var stop = $(".puse-control[rel='puse']", this);
+  
+        stop.click(function() {
+  
+          clearInterval(interval);
+        })
+  
+      });
+  
+    };
+  })(jQuery);
+  
---
+++ lip.js
@@ -0,0 +1,10 @@
+$("#slideshow").slider({ 
+
+    // to do adjust setting .
+
+    // fadeOut:50000,
+    // fadeIn:40000,
+    // time:10000
+
+ });
+
